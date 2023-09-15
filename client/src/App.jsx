e4367b0a8b7d9fcbd8d7fee76682699c07e9b5ce
MokhammadVu <stechkin_95@hotmail.com>
2023-09-15 09:26:15 +0200

    uplooad

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react"
+
+import { 
+  HashRouter as Router,
+  Routes,
+  Route,
+ } from "react-router-dom"
+
+import  LandingPage  from "./routes/LandindPage"
+import Navbar from "./components/Navbar/Navbar"
+import SignUpPage from "./routes/signUp/SignUpPage"
+import LoginPage from "./routes/LogIn/LoginPage"
+import Footer from "./components/footer/Footer"
+import { UserProvider } from "./UserContext"
+
+
+function App() {
+
+  return (
+    <UserProvider>
+        <Router>
+          <Navbar/>
+          <Routes>
+          <Route path="/" element={<LandingPage />}/>
+          <Route path="/login" element={<LoginPage />}/>
+          <Route path="/signup" element={<SignUpPage />}/>
+          </Routes>
+          <Footer/>
+        </Router>
+    </UserProvider>
+  )
+}
+
+export default App
---
+++ client/src/routes/RentCar/RentCarPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./rebt.module.css";
+import { useState } from "react";
+
+
+function RentCarPage() {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default RentCarPage;
