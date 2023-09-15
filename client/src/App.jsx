5fd0ff58cf78ffb794b2bf25b0e8d6cc72c05fe0
MokhammadVu <stechkin_95@hotmail.com>
2023-09-15 10:38:03 +0200

    up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
           <Navbar/>
           <Routes>
           <Route path="/" element={<LandingPage />}/>
+          <Route path="/" element={<LandingPage />}/>
           <Route path="/login" element={<LoginPage />}/>
           <Route path="/signup" element={<SignUpPage />}/>
           </Routes>
